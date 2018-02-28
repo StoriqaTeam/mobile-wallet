4671417b9984764b3140d60221d6a3c272eeead6
Alexander Korovkin <greenteamer@bk.ru>
2018-02-28 13:10:40 +0300

    crypting work well

--- App.js
+++ App.js
@@ -12,36 +12,29 @@ import {
   Text,
   View,
   Button,
+  TextInput,
 } from 'react-native';
+import Aes from 'react-native-aes-crypto';
 
 
 const Web3 = require('web3');
-// const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/fbuouJvwnJedVLF6og25'));
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+// const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/fbuouJvwnJedVLF6og25'));
 
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
+
 export default class App extends Component<Props> {
+  constructor() {
+    super();
+    this.state = {
+      result: null,
+      pin: null,
+    }
+  }
+
   componentWillMount() {
     web3.eth.getBlock('latest').then(console.log);
-    const createAccount = web3.eth.accounts.create();
-    console.log('*** create account: ', createAccount);
-    this.getData(web3);
-    setTimeout(() => {
-      web3.eth.getAccounts().then(result => console.log('*** get accounts after timeout: ', result));
-    }, 2000);
-    // console.log('*** App accounts: ', web3.eth.getAccounts().then(result => console.log('*** App getAccounts() result: ', result)));
-    // const accounts = web3.eth.accounts.create();
-    // const wallet = web3.eth.accounts.wallet.create();
-
-    console.log('*** App wallet: ', web3.eth.accounts.wallet);
   }
 
   getData = async () => {
@@ -61,21 +54,45 @@ export default class App extends Component<Props> {
   newAccount = async () => {
     const newAccount = web3.eth.accounts.create();
     const wallet = web3.eth.accounts.wallet.add(newAccount);
-    console.log('*** App new account: ', newAccount);
-    console.log('*** App new wallet: ', web3.eth.accounts.wallet);
+  }
+
+  onChangePin = (pin) => {
+    this.setState({ pin });
+  }
+
+  generateBase64Key = async () => {
+    const { pin } = this.state;
+    const salt = Math.random().toString(36).substring(2, 15);
+    return await Aes.pbkdf2(pin, salt);
+  }
+
+  encryptPrivat = async () => {
+    const text = "Hello some text";
+    const keyBase64 = await this.generateBase64Key();
+    const ivHex = convertToHex(randomString(16));
+    // const iv = "11112222333344445555666677778888";
+    // const salt = Math.random().toString(36).substring(2, 15);
+    // console.log('*** check Base64: ', isBase64(ivBase64), isBase64(keyBase64));
+    // console.log('*** App encryptPrivat data: ', {text, keyBase64, ivHex});
+    const encryptedResult = await Aes.encrypt(text, keyBase64, ivHex);
+    // keyBase64 = cece3a7dc9cf86aae926fd2ee520a06e
+    // ivHex = 11112222333344445555666677778888
+    // console.log('*** App encryptedResult: ', encryptedResult);
+    return encryptedResult;
+  }
+
+  handleEncript = () => {
+    this.encryptPrivat().then(result => {
+      // this.setState({ result });
+    });
   }
 
   render() {
+    const { pin, result } = this.state;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
+      <View style={{}}>
         <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+          {result}
         </Text>
         <Button
           onPress={this.getData}
@@ -95,6 +112,20 @@ export default class App extends Component<Props> {
         <Button
           onPress={this.newAccount}
           title="New Account"
+          color="#841584"
+        />
+        <TextInput
+          value={pin}
+          onChangeText={this.onChangePin}
+          keyboardType="numeric"
+          maxLength={5}
+          enablesReturnKeyAutomatically={true}
+          returnKeyType="done"
+          style={{ margin: 16, padding: 5, borderWidth: 1 }}
+        />
+        <Button
+          onPress={this.handleEncript}
+          title="Generate key"
           color="#841584"
         />
       </View>
@@ -120,3 +151,31 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+function randomString(length) {
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+	var randomstring = '';
+	for (var i=0; i<length; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum, rnum+1);
+  }
+	return randomstring;
+}
+
+function isBase64(str) {
+  try {
+      return btoa(atob(str)) == str;
+  } catch (err) {
+      return false;
+  }
+}
+
+function convertToHex(str) {
+  var hex = '';
+  for(var i=0;i<str.length;i++) {
+      hex += ''+str.charCodeAt(i).toString(16);
+  }
+  console.log('*** str: ', str);
+  console.log('*** hex: ', hex);
+  return hex;
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# STQ exchanger
+
+## Install
+
+## Troubleshooting
+* install react-native-aes from https://github.com/StoriqaTeam/react-native-aes.git
